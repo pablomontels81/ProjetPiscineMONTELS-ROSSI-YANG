bb9117ed72c9ed4c87cba1b3fa9321c1b30692de
antoine2899 <antoine.rossi@edu.ece.fr>
2019-04-16 11:15:28 +0200

    16/04 11h15 Antoine

---
+++ arete.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include<unordered_map>
+#include<unordered_set>
+#include "arete.h"
+
+Arete::Arete(std::string id,float cout1,float cout2,double S1,double S2):m_id{id},m_cout1{cout1},m_cout2{cout2},m_sommetdepart{S1},m_sommetarrive{S2}
+{
+}
+
+void Arete::afficherDataArete() const
+{
+    std::cout<<m_id<<"  "<<m_sommetdepart<<"  "<<m_sommetarrive<<std::endl;
+}
+
+Arete::~Arete()
+{
+    //dtor
+}
